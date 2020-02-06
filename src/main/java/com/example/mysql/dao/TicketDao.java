c8ffd1326b52d596bafc68deed570642ea51e37e
91913 <91913@DESKTOP-TI4ASDV>
2020-02-06 19:27:29 +0530

    remove error and warning messages

--- src/main/java/com/example/mysql/dao/TicketDao.java
+++ src/main/java/com/example/mysql/dao/TicketDao.java
@@ -1,11 +1,9 @@
 package com.example.mysql.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 import com.example.mysql.model.Ticket;
 
-
-public interface TicketDao extends JpaRepository<Ticket, Integer>{
+public interface TicketDao extends JpaRepository<Ticket, Integer> {
 
 }
